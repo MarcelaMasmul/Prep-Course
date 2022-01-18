56d33d7d80ed76e052144f508b346d31ca3c6ac6
MarcelaMasmul <you@example.com>
2022-01-18 09:08:47 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = Tomas ;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 5;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1 ;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -32,17 +32,29 @@ function devolverString(str) {
 }
 
 function suma(x, y) {
+  var suma = x + y;
+  return suma;
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
   
 }
+sumarDosNumeros(5, 5); // 10
+ console.log(suma (5,5))
+
+
 
 function resta(x, y) {
+   var Resta = x - y;
+   return resta;
+
   // Resta "y" de "x" y devuelve el valor
-  // Tu código:
+  // Tu código:  
   
 }
+resta(5, 1);// 4
+
+
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
